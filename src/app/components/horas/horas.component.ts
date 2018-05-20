9c58fb904c07f9c9f652aafb736d224bf518e873
Genoshan <razerdude@gmail.com>
2018-05-20 18:29:48 -0300

    Desarrollo de Formulario Carga de Horas

--- src/app/components/horas/horas.component.ts
+++ src/app/components/horas/horas.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgDatepickerModule } from 'ng2-datepicker';
+import { DatepickerOptions } from 'ng2-datepicker';
+import * as frLocale from 'date-fns/locale/fr';
+
 @Component({
   selector: 'app-horas',
   templateUrl: './horas.component.html',
   styles: []
 })
 export class HorasComponent implements OnInit {
+
+  options: DatepickerOptions = {
+    minYear: 1970,
+    maxYear: 2030,
+    displayFormat: 'MMM D[,] YYYY',
+    barTitleFormat: 'MMMM YYYY',
+    dayNamesFormat: 'dd',
+    firstCalendarDay: 0, // 0 - Sunday, 1 - Monday
+    locale: frLocale,
+    minDate: new Date(Date.now()), // Minimal selectable date
+    maxDate: new Date(Date.now()),  // Maximal selectable date
+    barTitleIfEmpty: 'Click to select a date'
+  };
 
   constructor() { }
 
