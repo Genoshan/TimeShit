a16cd98d24a9765960d0c9b47f7ff4409d45ee7f
Genoshan <razerdude@gmail.com>
2018-05-27 16:41:13 -0300

    Nuevo Proyecto Inline + Actualizacion de datos de proyecto

--- src/app/components/proyecto/proyecto.component.ts
+++ src/app/components/proyecto/proyecto.component.ts
@@ -14,45 +14,32 @@ import { ProyectosService } from '../../services/proyectos.service';
 })
 export class ProyectoComponent implements OnInit{
 
-  options: DatepickerOptions = {
-    minYear: 1970,
-    maxYear: 2030,
-    displayFormat: 'MMM D[,] YYYY',
-    barTitleFormat: 'MMMM YYYY',
-    dayNamesFormat: 'dd',
-    firstCalendarDay: 0, // 0 - Sunday, 1 - Monday
-    locale: frLocale,
-    minDate: new Date(Date.now()), // Minimal selectable date
-    maxDate: new Date(Date.now()),  // Maximal selectable date
-    barTitleIfEmpty: 'Click to select a date'
-  };
-
     proyecto:Proyecto= {
 
-      nombre:"",
+      //nombre:"",
       fechaInicio:new Date(Date.now()),
       Estado:true,
       codigoProyecto:"",
-      
-    id: 0,
-    create_date: new Date(Date.now()),
-    write_date: new Date(Date.now()),        
-    alias_model: "",
-    alias_id:0,	
-    write_uid:0,	
-    effective_hours: 0,
-    planned_hours: 0,
-    active:true,
-    analytic_account_id: 0, 
-    create_uid:0,
-    progress_rate: 0,
-    sequence: 0,	
-    privacy_visibility: "",	
-    total_hours: 0,	
-    state: "",	
-    project_code: "",
-    project_escalation_id:0, 
-    project_project_resource_calendar_id: 0
+      id: 0,
+
+    // create_date: new Date(Date.now()),
+    // write_date: new Date(Date.now()),        
+    // alias_model: "",
+    // alias_id:0,	
+    // write_uid:0,	
+    // effective_hours: 0,
+    // planned_hours: 0,
+    // active:true,
+    // analytic_account_id: 0, 
+    // create_uid:0,
+    // progress_rate: 0,
+    // sequence: 0,	
+    // privacy_visibility: "",	
+    // total_hours: 0,	
+    // state: "",	
+    // project_code: "",
+    // project_escalation_id:0, 
+    // project_project_resource_calendar_id: 0
   }
 
 
@@ -62,6 +49,8 @@ export class ProyectoComponent implements OnInit{
     console.log(this.proyecto);
     this.pr.crearProyectos(this.proyecto);
   }
+
+
 
   ngOnInit() {
   }
--- src/app/interfaces/proyecto.ts
+++ src/app/interfaces/proyecto.ts
@@ -1,94 +1,41 @@
 export interface Proyecto {
 
-
-
     /*
     ESTRUCTURA DE DATOS DE PROYECTOS
     */
 
-    nombre:string;
+    //nombre:string;
     fechaInicio:Date;
     Estado:boolean;
     codigoProyecto:string;
+    id: number;
 
 
-    id: number; 
-    create_date: Date; /*IS 'Created on*/
-    write_date: Date;  /*IS 'Last Updated on'*/      
-    alias_model: string; /*Alias Model' - Siempre "project.task"*/
-    alias_id:number;	/* IS 'Alias' - (mail_alias) - el id de mail asociado al usuario actual*/
-    write_uid:number;	/*IS 'Last Updated by' (res_users)*/
-    effective_hours: number; /*IS 'Time Spent'*/
-    planned_hours: number; /*IS 'Planned Time'*/
-    active:boolean; /*IS 'Active'*/
-    create_uid:number; /*IS 'Created by' (res_users)	*/
-    progress_rate: number; /*IS 'Progress';*/
-    sequence: number;	/*IS 'Sequence';*/
-    privacy_visibility: string;	/*IS 'Privacy / Visibility';*/
-    total_hours: number; /* IS 'Total Time'*/	
-    state: string;	/*IS 'Status'*/
-    project_code: string; /*IS 'Codigo de Proyecto';*/
-
-    /*Objeto Empresa o Cliente o Compañía para mostrar nombre en base a Id
-    IS 'Contract/Analytic'(account_analytic_account)-el id de compañia*/
-    analytic_account_id: number; 
     
-    //están en null
-    project_escalation_id:number; /*IS 'Project Escalation';(project_project)*/
-    project_project_resource_calendar_id: number; /*IS 'Working Time'; (resource_calendar)*/
+    // create_date: Date; /*IS 'Created on*/
+    // write_date: Date;  /*IS 'Last Updated on'*/      
+    // alias_model: string; /*Alias Model' - Siempre "project.task"*/
+    // alias_id:number;	/* IS 'Alias' - (mail_alias) - el id de mail asociado al usuario actual*/
+    // write_uid:number;	/*IS 'Last Updated by' (res_users)*/
+    // effective_hours: number; /*IS 'Time Spent'*/
+    // planned_hours: number; /*IS 'Planned Time'*/
+    // active:boolean; /*IS 'Active'*/
+    // create_uid:number; /*IS 'Created by' (res_users)	*/
+    // progress_rate: number; /*IS 'Progress';*/
+    // sequence: number;	/*IS 'Sequence';*/
+    // privacy_visibility: string;	/*IS 'Privacy / Visibility';*/
+    // total_hours: number; /* IS 'Total Time'*/	
+    // state: string;	/*IS 'Status'*/
+    // project_code: string; /*IS 'Codigo de Proyecto';*/
 
-    /*
-    ESTOS HAY QUE ASIGNARLOS POR ATRAS Y NO POR FRONT AL CREAR EL PROYECTO.
-    write_date: new Date(Date.now()),  -      
-    alias_model: "", - 
-    alias_id:0,	- el usuario actual
-    write_uid:0, -	
-    effective_hours: 0, -        
-    create_uid:0, -
-    progress_rate: 0, -
-    sequence: 0,	
-    privacy_visibility: "",	
-    total_hours: 0,	-
-    analytic_account_id: 0, -?
-    project_escalation_id:0, ?
-    project_project_resource_calendar_id: 0 ?
+    // /*Objeto Empresa o Cliente o Compañía para mostrar nombre en base a Id
+    // IS 'Contract/Analytic'(account_analytic_account)-el id de compañia*/
+    // analytic_account_id: number; 
     
-    */
+    // //están en null
+    // project_escalation_id:number; /*IS 'Project Escalation';(project_project)*/
+    // project_project_resource_calendar_id: number; /*IS 'Working Time'; (resource_calendar)*/
+
+    
 }
-
-/*
-   nombre:string;
-    //nota:string;
-    canthora:number;
-    tipoProyecto:string;
-    key$?:string;
-    empresa:string;
-*/
-
-/*PROYECTO:
-id - int	
-create_date - datetime	
-Write_date - datetime	
-alias_model -	string
-Alias_id - int	(mail_alias)
-Write_uid - int	(res_users)
-Effective_hours - double	
-Planned_hours - double	
-active	- boolean
-analytic_account_id	- int (account_analytic_account)
-Create_uid - int (res_users)	
-progress_rate	- double
-Sequence - int	
-Privacy_visibility - string	
-Total_hours - double	
-State - string	
-Project_code - string
-Project_escalation_id - integer (project_project)
-Project_project_resource_calendar_id (resource_calendar)	
-
---------------------------------------
-PARA FORMATEAR A DATETIME:
-var today = $filter('date')(new Date(),'yyyy-MM-dd HH:mm:ss Z');
----------------------------------------
-*/
 
